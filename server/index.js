ee2bde76c65dcc26046fdd888d4e6abb4aadb6f2
Emerson Dias Machado <edias7942@gmail.com>
2023-04-03 15:28:38 -0300

    commit

--- server/index.js
+++ server/index.js
@@ -3,28 +3,49 @@ const app = express()
 const mysql = require("mysql")
 const cors = require("cors")
 
+let DataBaseChoose = "cloud"
+
 app.use(express.json())
 app.use(cors())
 
-const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "jwccc"
-})
+let db
+// Conexão com o Banco de Dados em Nuvem:
+if (DataBaseChoose === "cloud") {
+    console.log("Conectado ao Banco de Dados: Planet Scale")
+    db = mysql.createConnection({
+        database: "jw-organization",
+        user: "0sne07cwjouq446ozsn1",
+        host: "aws.connect.psdb.cloud",
+        password: "pscale_pw_3bqoWpf0JzoAVDrrBib14FFbSp6EEqgIibHwscxOywJ",
+        ssl: {
+            rejectUnauthorized: true
+        }
+    });
+}
 
-console.log("Conectado ao banco de dados!")
+// Conexão com o Banco de Dados Local:
+if (DataBaseChoose === "local") {
+    console.log("Conectado ao banco de dados Local da máquina!")
+    db = mysql.createPool({
+        host: "localhost",
+        user: "root",
+        password: "1234",
+        database: "jwccc"
+    })
+}
+
 
 app.post("/designation", (req, res) => {
     const week = req.body.week
     const positionInWeek = req.body.positionInWeek
     db.query
-        (`SELECT ID, PLACE, TIME, DESIGNATED_1, DESIGNATED_2 FROM designations2023 WHERE WEEK = ${week} AND P_IN_WEEK = ${positionInWeek}`,
+        (`SELECT ID, PLACE, TIME, DESIGNATED_1, DESIGNATED_2 FROM DESIGNATIONS2023 WHERE WEEK = ${week} AND P_IN_WEEK = ${positionInWeek}`,
             (err, result) => {
                 if (err) res.send(err)
                 res.send(result)
             })
 })
+
 
 app.post("/designate", (req, res) => {
     const id = req.body.id
@@ -38,29 +59,30 @@ app.post("/designate", (req, res) => {
     parseInt(time)
 
     db.query
-    (`UPDATE designations2023 SET PLACE = "${place}", TIME = "${time}", DESIGNATED_1 = "${designated1}", DESIGNATED_2 = "${designated2}" WHERE ID = ${id}`)
+        (`UPDATE DESIGNATIONS2023 SET PLACE = "${place}", TIME = "${time}", DESIGNATED_1 = "${designated1}", DESIGNATED_2 = "${designated2}" WHERE ID = ${id}`)
+
 })
 
 
 
 app.post("/cart_places", (req, res) => {
     db.query
-    ("SELECT * FROM cart_places ORDER BY PLACE",
-        (err, result) => {
-            if (err) res.send(err)
-            res.send(result)
-        })
+        ("SELECT * FROM cart_places ORDER BY PLACE",
+            (err, result) => {
+                if (err) res.send(err)
+                res.send(result)
+            })
 })
 
 
 
 app.post("/territories", (req, res) => {
     db.query
-    ("SELECT * FROM territories ORDER BY NUMBER",
-    (err, result) => {
-        if (err) res.send(err)
-        res.send(result)
-    })
+        ("SELECT * FROM territories ORDER BY NUMBER",
+            (err, result) => {
+                if (err) res.send(err)
+                res.send(result)
+            })
 })
 
 
--- teste.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const mysql = require('mysql2')
-DATABASE_URL='mysql://76f0it2t12spt0ad1g6q:pscale_pw_v9w6Tk9lEzbf2fpkBGaLTYKVji5NIeg1ODAMJFxmJer@aws.connect.psdb.cloud/jw-organization?ssl={"rejectUnauthorized":true}'
-const connection = mysql.createConnection(DATABASE_URL)
-let teste = connection.execute("SELECT * FROM DESIGNATIONS2023")
-console.log(teste)
-console.log('Connected to PlanetScale!')
-connection.end()
