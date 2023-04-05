7865731d22da32928ac8d3c37f9af2e1c0fe3033
unknown <edias7942@gmail.com>
2023-04-05 13:34:16 -0300

    Deleting the server past

--- backup.sql
+++
@@ -1,7 +0,0 @@
-insert into cart_places values
-(1,"Cond. Valdir",NULL,NULL,NULL,NULL,NULL),
-(2,"Feira Soutomaior",NULL,NULL,NULL,NULL,NULL),
-(3,"Posto de Saúde",NULL,NULL,NULL,NULL,NULL),
-(4,"Pq. dos Sonhos",NULL,NULL,NULL,NULL,NULL),
-(5,"Bandeirantes",NULL,NULL,NULL,NULL,NULL),
-(6,"Rua do Israel",NULL,NULL,NULL,NULL,NULL);
